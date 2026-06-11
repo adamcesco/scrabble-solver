484868e75772b9c96d12457e39fd6062b7b2b56e
adamcesco <adamcrescobedo@gmail.com>
2026-06-10 20:30:56 -0500

    Implement cache optimizations

--- src/main.c
+++ src/main.c
@@ -40,19 +40,18 @@ int main(int argc, char **argv)
     Row row_with_newly_added_word = {0};
     Board copy_of_original_board = board;
     // for each row in the board
-    for (uint8_t row_index = 0; row_index < BOARD_SIZE; ++row_index) {
-        // for each word in the dictionary
-        for (size_t entry_index = 0; entry_index < starting_position_to_row.count; ++entry_index) {
-            const WordStartRowEntry *entry = &starting_position_to_row.entries[entry_index];
+    // for each word in the dictionary
+    for (size_t entry_index = 0; entry_index < starting_position_to_row.count; ++entry_index) {
+        const WordStartRowEntry *entry = &starting_position_to_row.entries[entry_index];
+        
+        // for each possible placement of that word in a row on the board
+        for (uint8_t start = 0; start < BOARD_SIZE; ++start) {
+            if ((entry->valid_starts & (uint16_t)(UINT16_C(1) << start)) == 0) {
+                break; // this break means that the rows in a WordStartRowEntry need to slide down the row in the 0 to 15 direction
+            }
+            const Row *row_with_just_proposed_word = &entry->rows[start];
 
-            // for each possible placement of that word in a row on the board
-            for (uint8_t start = 0; start < BOARD_SIZE; ++start) {
-                if ((entry->valid_starts & (uint16_t)(UINT16_C(1) << start)) == 0) {
-                    continue;
-                }
-                
-                const Row *row_with_just_proposed_word = &entry->rows[start];
-
+            for (uint8_t row_index = 0; row_index < BOARD_SIZE; ++row_index) {
                 if (is_placeable_on_row(&board.rows[row_index], row_with_just_proposed_word)) {
                     if (place_word_onto_perpendicular_rows_and_validate(
                         &dictionary,
@@ -70,8 +69,8 @@ int main(int argc, char **argv)
                         board.rows[row_index] = row_with_newly_added_word;
                         // now `board` is the new version of the board that has the new word placed within it
 
-                        printf("row %2d ", row_index + 1);
-                        print_row(board.rows[row_index]);
+                        // printf("row %2d ", row_index + 1);
+                        // print_row(board.rows[row_index]);
 
                         // restoring the board back to it's original state
                         board.rows[row_index] = copy_of_original_board.rows[row_index];
